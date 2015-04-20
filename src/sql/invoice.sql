7a1245f430e862e2a4b40cc75e904b8ab1036be1
felix1429 <thennig314@gmail.com>
2015-04-20 15:11:36 -0500

    use product/customer/venue code instead of id as fk in sql

--- src/sql/invoice.sql
+++ src/sql/invoice.sql
@@ -110,16 +110,16 @@ DROP TABLE IF EXISTS `InvoiceProduct`;
 /*!40101 SET character_set_client = utf8 */;
 
 CREATE TABLE `InvoiceProduct` (
-  `ProductID`     INT(11) NOT NULL,
-  `InvoiceID`     INT(11) NOT NULL,
+  `ProductCode`   VARCHAR(30) NOT NULL,
+  `InvoiceID`     INT(11)     NOT NULL,
   `NumberOfHours` VARCHAR(30),
   `NumberOfUnits` VARCHAR(30),
   `BeginDate`     VARCHAR(30),
   `EndDate`       VARCHAR(30),
   `Date`          VARCHAR(30),
   `Seats`         VARCHAR(30),
-  PRIMARY KEY (`ProductID`),
-  FOREIGN KEY (`ProductID`) REFERENCES Product (`ID`),
+  PRIMARY KEY (`ProductCode`),
+  FOREIGN KEY (`ProductCode`) REFERENCES Product (`Code`),
   FOREIGN KEY (`InvoiceID`) REFERENCES Invoice (`ID`)
 )
   ENGINE = InnoDB
@@ -145,10 +145,10 @@ DROP TABLE IF EXISTS `Email`;
 
 CREATE TABLE `Email` (
   `EmailID`      INT(11) NOT NULL AUTO_INCREMENT,
-  `PersonID`     INT(11) NOT NULL,
+  `PersonCode`   INT(11) NOT NULL,
   `EmailAddress` VARCHAR(300)     DEFAULT NULL,
   PRIMARY KEY (`EmailID`),
-  FOREIGN KEY (`PersonID`) REFERENCES Person (`PersonID`)
+  FOREIGN KEY (`PersonCode`) REFERENCES Person (`PersonCode`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
@@ -164,12 +164,18 @@ CREATE TABLE `Email` (
 
 /*--LOCK TABLES `Email` WRITE;*/;
 /*!40000 ALTER TABLE `Email` DISABLE KEYS */;
-INSERT INTO `Email` VALUES(1,1,'scastro@cubs.com'),(2,1,'starlin_castro13@gmail.com'),(3,2,'brock_f_sampson@gmail.com'),(4,2,'bsampson@venture.com'),(5,3,'obrien@ds9.com'),
-  (6,3,'obrien@enterprise.gov'),(7,4,''),(8,5,''),(9,6,'bfox@gmail.com'),(10,6,'csheen@crazy.net'),(11,7,'sveum@cub.com'),(12,8,'whartnell@doctors.com'),(13,8,'dr@who.com'),
-  (14,9,'ptroug@cse.unl.edu'),(15,9,'ptrou32@unl.edu'),(16,10,'jpet@whofan.com'),(17,11,'famousdoc@who.com'),(18,11,'tbaker@cse.unl.edu'),(19,11,'mostfamous@whovian.com'),
-  (20,11,'thedoctor@bbc.com'),(21,12,'rhurndall@cse.unl.edu'),(22,12,'richard@unl.edu'),
-  (23,13,'dr@baker.com'),(24,14,'slyguy@hotmail.com'),(25,14,'mccoy@whofan.com'),(26,15,'pmcgann@mlb.com,foo@bar.com'),(27,15,'pmc@unl.edu'), (28,16,'newguy@whovian.com'),
-  (29,17,'actor@shakespeare.com'),(30,17,'tdavid@unl.com'),(31,18,'msmith@who.com'),(32,18,'thedoc@cse.unl.edu'),(33,19,''),(34,20,'');
+INSERT INTO `Email` VALUES (1, '944c', 'scastro@cubs.com'), (2, '944c', 'starlin_castro13@gmail.com'),
+  (3, '306a', 'brock_f_sampson@gmail.com'), (4, '306a', 'bsampson@venture.com'), (5, '55bb', 'obrien@ds9.com'),
+  (6, '55bb', 'obrien@enterprise.gov'), (7, '2342', ''), (8, 'aef1', ''), (9, '321f', 'bfox@gmail.com'),
+  (10, '321f', 'csheen@crazy.net'), (11, 'ma12', 'sveum@cub.com'), (12, '321nd', 'whartnell@doctors.com'),
+  (13, '321nd', 'dr@who.com'),
+  (14, 'nf32a', 'ptroug@cse.unl.edu'), (15, 'nf32a', 'ptrou32@unl.edu'), (16, '321na', 'jpet@whofan.com'),
+  (17, '231', 'famousdoc@who.com'), (18, '231', 'tbaker@cse.unl.edu'), (19, '231', 'mostfamous@whovian.com'),
+  (20, '231', 'thedoctor@bbc.com'), (21, '6doc', 'rhurndall@cse.unl.edu'), (22, '6doc', 'richard@unl.edu'),
+  (23, '321dr', 'dr@baker.com'), (24, '1svndr', 'slyguy@hotmail.com'), (25, '1svndr', 'mccoy@whofan.com'),
+  (26, '123lst', 'pmcgann@mlb.com,foo@bar.com'), (27, '123lst', 'pmc@unl.edu'), (28, 'nwdoc1', 'newguy@whovian.com'),
+  (29, '2ndbestd', 'actor@shakespeare.com'), (30, '2ndbestd', 'tdavid@unl.com'), (31, 'wrddoc', 'msmith@who.com'),
+  (32, 'wrddoc', 'thedoc@cse.unl.edu'), (33, 'bbchar', ''), (34, 'doc05', '');
 /*!40000 ALTER TABLE `Email` ENABLE KEYS */;
 /*--UNLOCK TABLES;*/;
 
@@ -196,20 +202,37 @@ CREATE TABLE `Address` (
   COLLATE = utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `Address` VALUES(1,'1 MetLife Stadium Drive','East Rutherford','NJ','07073','USA'),(2,'1 AT&T Way','Arlington','TX','76011','USA'),
-  (3,'1 Arrowhead Drive','Kansas City','MO','64129','USA'),(4,'347 Don Shula Dr','Miami Gardens','FL','33056','USA'),
-  (5,'800 South Mint Street','Charlotte','NC','28202','USA'),(6,'100 Alfred Lerner Way','Cleveland','OH','44114','USA'),
-  (7,'1410 Museum Campus Drive','Chicago','IL','60605','USA'),(8,'800 Occidental Avenue South','Seattle','WA','98134','USA'),
-  (9,'1 Cardinals Drive','Glendale','AZ','85305','USA'),(10,'1 Georgia Dome Drive Northwest','Atlanta','GA','30313','USA'),
-  (11,'1060 West Addison Street','Chicago','IL','60613','USA'),(12,'123 N 1st Street','Omaha','NE','68116','USA'),(13,'8753 West 3rd Ave.','Dallas','TX','75001','USA'),
-  (14,'123 Friendly St','Ottawa','ON','K1A 0G9','Canada'),(15,'1 Wall Street','New York','NY','10005-0012','USA'),(16,'321 Bronx Street','New York','NY','10004','USA'),
-  (17,'1060 West Addison Street','Chicago','IL','60613','USA'),(18,'1060 West Addison Street','Chicago','IL','60613','USA'),
-  (19,'301 Front St W','Toronto','ON','M5V 2T6','Canada'),(20,'1 Blue Jays Way','Toronto','ON','M5V 1J1','Canada'),(21,'Campos E1290','Mexico City','FD','NULL','Mexico'),
-  (22,'Avery Hall','Lincoln','NE','68503','USA'),(23,'126-01 Roosevelt Ave','Flushing','NY','11368','USA'),(24,'1 Metlife Stadium Dr','East Rutherford','NJ','07073','USA'),
-  (25,'1 E 161st St','Bronx','NY','10451','USA'),(26,'700 E Grand Ave','Chicago','IL','60611','USA'),(27,'800 West 7th Street','Albuquerque','NM','87105','USA'),
-  (28,'123 Cabo San Lucas','Los Cabos','BCS',' ','Mexico'),(29,'259 Concorde Suites','Lincoln','NE','68588-0115','USA'),(30,'184 Marvel Way','New York','NY','10453','USA'),
-  (31,'123 Venture Way','Culver City','CA','90230','USA'),(32,'9800 Savage Rd','Fort Meade','MD','20755','USA'),(33,'1060 West Addison','Chicago','IL','60601','USA'),
-  (34,'456 West 7th St.','Omaha','NE','68500','USA')
+INSERT INTO `Address` VALUES (1, '1 MetLife Stadium Drive', 'East Rutherford', 'NJ', '07073', 'USA'),
+  (2, '1 AT&T Way', 'Arlington', 'TX', '76011', 'USA'),
+  (3, '1 Arrowhead Drive', 'Kansas City', 'MO', '64129', 'USA'),
+  (4, '347 Don Shula Dr', 'Miami Gardens', 'FL', '33056', 'USA'),
+  (5, '800 South Mint Street', 'Charlotte', 'NC', '28202', 'USA'),
+  (6, '100 Alfred Lerner Way', 'Cleveland', 'OH', '44114', 'USA'),
+  (7, '1410 Museum Campus Drive', 'Chicago', 'IL', '60605', 'USA'),
+  (8, '800 Occidental Avenue South', 'Seattle', 'WA', '98134', 'USA'),
+  (9, '1 Cardinals Drive', 'Glendale', 'AZ', '85305', 'USA'),
+  (10, '1 Georgia Dome Drive Northwest', 'Atlanta', 'GA', '30313', 'USA'),
+  (11, '1060 West Addison Street', 'Chicago', 'IL', '60613', 'USA'),
+  (12, '123 N 1st Street', 'Omaha', 'NE', '68116', 'USA'), (13, '8753 West 3rd Ave.', 'Dallas', 'TX', '75001', 'USA'),
+  (14, '123 Friendly St', 'Ottawa', 'ON', 'K1A 0G9', 'Canada'),
+  (15, '1 Wall Street', 'New York', 'NY', '10005-0012', 'USA'),
+  (16, '321 Bronx Street', 'New York', 'NY', '10004', 'USA'),
+  (17, '1060 West Addison Street', 'Chicago', 'IL', '60613', 'USA'),
+  (18, '1060 West Addison Street', 'Chicago', 'IL', '60613', 'USA'),
+  (19, '301 Front St W', 'Toronto', 'ON', 'M5V 2T6', 'Canada'),
+  (20, '1 Blue Jays Way', 'Toronto', 'ON', 'M5V 1J1', 'Canada'),
+  (21, 'Campos E1290', 'Mexico City', 'FD', 'NULL', 'Mexico'),
+  (22, 'Avery Hall', 'Lincoln', 'NE', '68503', 'USA'), (23, '126-01 Roosevelt Ave', 'Flushing', 'NY', '11368', 'USA'),
+  (24, '1 Metlife Stadium Dr', 'East Rutherford', 'NJ', '07073', 'USA'),
+  (25, '1 E 161st St', 'Bronx', 'NY', '10451', 'USA'), (26, '700 E Grand Ave', 'Chicago', 'IL', '60611', 'USA'),
+  (27, '800 West 7th Street', 'Albuquerque', 'NM', '87105', 'USA'),
+  (28, '123 Cabo San Lucas', 'Los Cabos', 'BCS', ' ', 'Mexico'),
+  (29, '259 Concorde Suites', 'Lincoln', 'NE', '68588-0115', 'USA'),
+  (30, '184 Marvel Way', 'New York', 'NY', '10453', 'USA'),
+  (31, '123 Venture Way', 'Culver City', 'CA', '90230', 'USA'),
+  (32, '9800 Savage Rd', 'Fort Meade', 'MD', '20755', 'USA'),
+  (33, '1060 West Addison', 'Chicago', 'IL', '60601', 'USA'),
+  (34, '456 West 7th St.', 'Omaha', 'NE', '68500', 'USA')
 
 /*--UNLOCK TABLES;*/;
 
@@ -230,7 +253,7 @@ CREATE TABLE `Customer` (
   `PersonID`     INT(11)     NOT NULL,
   `CustomerName` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`CustomerID`),
-  FOREIGN KEY (`PersonID`) REFERENCES Person (`PersonID`)
+  FOREIGN KEY (`PersonCode`) REFERENCES Person (`PersonCode`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   FOREIGN KEY (`AddressID`) REFERENCES Address (`AddressID`)
@@ -252,16 +275,16 @@ DROP TABLE IF EXISTS `Invoice`;
 /*!40101 SET character_set_client = utf8 */;
 
 CREATE TABLE `Invoice` (
-  `ID`          INT(11)     NOT NULL AUTO_INCREMENT,
-  `InvoiceCode` VARCHAR(40) NOT NULL,
-  `CustomerID`  INT(11)     NOT NULL,
-  `Date`        VARCHAR(40) NOT NULL,
-  `PersonID`    INT(11)     NOT NULL,
+  `ID`           INT(11)     NOT NULL AUTO_INCREMENT,
+  `InvoiceCode`  VARCHAR(40) NOT NULL,
+  `CustomerCode` VARCHAR(20) NOT NULL,
+  `Date`         VARCHAR(40) NOT NULL,
+  `PersonCode`   VARCHAR(40) NOT NULL,
   PRIMARY KEY (`ID`),
-  FOREIGN KEY (`CustomerID`) REFERENCES Customer (`CustomerID`)
+  FOREIGN KEY (`CustomerCode`) REFERENCES Customer (`CustomerCode`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (`PersonID`) REFERENCES Person (`PersonID`)
+  FOREIGN KEY (`PersonCode`) REFERENCES Person (`PersonCode`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
@@ -293,6 +316,7 @@ DROP TABLE IF EXISTS `Venue`;
 
 CREATE TABLE `Venue` (
   `VenueID`       INT(11)     NOT NULL AUTO_INCREMENT,
+  `VenueCode`     VARCHAR(40) NOT NULL,
   `AddressID`     INT(11)     NOT NULL,
   `VenueName`     VARCHAR(40) NOT NULL,
   `VenueCapacity` INT(11)     NOT NULL,
@@ -314,11 +338,13 @@ CREATE TABLE `Venue` (
 /*--LOCK TABLES `Venue` WRITE;*/;
 /*!40000 ALTER TABLE `Venue` DISABLE KEYS */;
 INSERT INTO `Venue`
-VALUES (1, 1, 'Metlife Stadium', 82566), (2, 2, 'AT&T Stadium', 80000), (3, 3, 'Arrowhead Stadium', 79541),
-  (4, 4, 'Sun Life Stadium', 65326),
-  (5, 5, 'Bank of America Stadium', 73779), (6, 6, 'FirstEnergy Stadium', 73204), (7, 7, 'Soldier Field', 61500),
-  (8, 8, 'CenturyLink Field', 67135),
-  (9, 9, 'University of Phoenix Stadium', 63400), (10, 10, 'Georgia Dome', 71228);
+VALUES (1, 'a001', 1, 'Metlife Stadium', 82566), (2, 'ai10', 2, 'AT&T Stadium', 80000),
+  (3, 'ai19', 3, 'Arrowhead Stadium', 79541),
+  (4, 'fl11', 4, 'Sun Life Stadium', 65326),
+  (5, 'c102', 5, 'Bank of America Stadium', 73779), (6, 'st329', 6, 'FirstEnergy Stadium', 73204),
+  (7, 'c181', 7, 'Soldier Field', 61500),
+  (8, 'w111', 8, 'CenturyLink Field', 67135),
+  (9, 'phx11', 9, 'University of Phoenix Stadium', 63400), (10, 'org59', 10, 'Georgia Dome', 71228);
 /*!40000 ALTER TABLE `Venue` ENABLE KEYS */;
 /*--UNLOCK TABLES;*/;
 
